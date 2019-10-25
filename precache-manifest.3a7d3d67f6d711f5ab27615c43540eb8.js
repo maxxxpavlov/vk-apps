c32970a38fd90e5d3ba86886940fea31b7fffe20
Maksim <elliot_hacker@protonmail.com>
2019-10-25 20:39:23 +0300

    Updates

---
+++ precache-manifest.3a7d3d67f6d711f5ab27615c43540eb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ef911c099a7027a599cac616b56fb1",
+    "url": "/vk-apps-tensorflow-example/index.html"
+  },
+  {
+    "revision": "cd174834b0def7843b8c",
+    "url": "/vk-apps-tensorflow-example/static/css/2.7214840c.chunk.css"
+  },
+  {
+    "revision": "cd174834b0def7843b8c",
+    "url": "/vk-apps-tensorflow-example/static/js/2.d1bfae26.chunk.js"
+  },
+  {
+    "revision": "fc639edf4554faa8afd9",
+    "url": "/vk-apps-tensorflow-example/static/js/main.3b926384.chunk.js"
+  },
+  {
+    "revision": "875f014660a1f666bf39",
+    "url": "/vk-apps-tensorflow-example/static/js/runtime-main.aeb91ddf.js"
+  }
+]);
